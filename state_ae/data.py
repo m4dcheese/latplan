c74fd83486b40e03541d5ba5c8bc7e980845f9de
Fabian Metschies <fabian.metschies@gmail.com>
2022-01-11 18:00:31 +0100

    Added data loader capability for deleting random single digits

--- state_ae/data.py
+++ state_ae/data.py
@@ -7,17 +7,29 @@ import torchvision.transforms as T
 from parameters import parameters
 
 class MNISTPuzzleDataset(Dataset):
-
-    def __init__(self, n: int = 1000, differing_digits: bool = False) -> None:
+    def __init__(self, n: int = 1000, differing_digits: bool = False, deletions: int = 0) -> None:
         super().__init__()
         self.n = n
+
+        # Fetch MNIST digits
         (images, labels), (_, _) = load_data()
+        # Group digit images by the actual digit
+        digit_classes = [images[labels == i] for i in range(10)]
+        digit_classes.append(np.zeros(digit_classes[0].shape).astype(np.uint8))
 
         # Generate logical puzzle permutations
         permutations = np.array([np.random.permutation(np.arange(9)) for i in range(n)])
-        index = np.random.randint(0, 10)
-        # Split digits into groups of equals
-        digit_classes = [images[labels == i] for i in range(10)]
+
+        if deletions > 0:
+            # Delete some digits
+            permutations, permutations_deletion = np.split(permutations, 2, axis=0)
+
+            masks = []
+            for dim in permutations_deletion.shape:
+                masks.append(np.random.choice(dim, size=deletions))
+
+            permutations_deletion[masks[0], masks[1]] = 10
+            permutations = np.append(permutations, permutations_deletion, axis=0)
 
         states = []
 
@@ -54,8 +66,24 @@ def get_loader(
     total_samples: int = 1000,
     batch_size: int = 100,
     differing_digits: bool = False,
+    deletions: int = 0,
     usecuda: bool = False
 ) -> DataLoader:
-    ds = MNISTPuzzleDataset(n=total_samples, differing_digits=differing_digits)
+    """
+    Returns a loader for the MNISTPuzzleDataset
+
+    Args:
+        total_samples:
+            Number of generated samples
+        batch_size:
+            Batch size for the DataLoader
+        differing_digits:
+            Whether or not to use different digits for puzzle generation. Default: false
+        deletions:
+            Total number of removed digits. The deletions only occur in one half of the whole dataset, so that one half of puzzle instances is complete.
+        usecuda:
+            Whether or not to use cuda
+    """
+    ds = MNISTPuzzleDataset(n=total_samples, differing_digits=differing_digits, deletions=deletions)
     loader = DataLoader(ds, batch_size=batch_size, pin_memory=usecuda)
     return loader
